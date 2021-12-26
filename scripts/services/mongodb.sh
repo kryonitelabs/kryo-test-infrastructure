30776bba020d5d05d4d6c70f8b270c5926b8bd58
Lukas Jost <lukas.jost@outlook.com>
2021-12-26 18:20:37 +0100

    update k8s and mongodb

--- scripts/services/mongodb.sh
+++ scripts/services/mongodb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
-helm upgrade -i mongodb bitnami/mongodb --version 10.30.6 -f ./configs/mongodb.yaml
+helm upgrade -i mongodb bitnami/mongodb --version 10.30.8 -f ./configs/mongodb.yaml
 
 while [ "$(kubectl get --ignore-not-found pods mongodb-0 -n default --no-headers -o jsonpath="{.status.phase}")" != "Running" ]; do
   echo "Waiting for MongoDB cluster nodes to be ready..."
